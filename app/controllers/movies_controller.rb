de2895dc2cbfded5241bb8cb62e8ed4d1d8432db
Forrest Hicks <forrest2405@live.com>
2016-03-06 03:45:57 +0000

    testing code out

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,7 +17,7 @@ class MoviesController < ApplicationController
     
     if params[:ratings].present?
       @user_ratings = Movie.where(rating: params[:ratings].keys)
-      @rating_settings = params[:ratings].keys
+      @rating_settings = params[:ratings]
     end
     
     if params[:sort].present?
@@ -27,10 +27,10 @@ class MoviesController < ApplicationController
     if @sort
       @movie = Movie.order(@sort)
       if @user_ratings
-        @movie = @movie.where(rating: @rating_setting) if params[:ratings].present?
+        @movie = @movie.where(rating: @rating_setting.keys) if params[:ratings].present?
       end
     elsif @user_ratings
-      @movie = Movie.where(rating: @rating_setting) if params[:ratings].present?
+      @movie = Movie.where(rating: @rating_setting.keys) if params[:ratings].present?
     else
       @movie = Movie.all
     end
